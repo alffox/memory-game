19b68d86b8a740039f4b44cfb0c03fedb369da3d
alffox <alfonso.crisci.it@gmail.com>
2017-10-26 12:59:23 +0200

    feat: If 2 cards match, print success message to the console

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ $('.col-md-3').click(function(clickOne) {
     $(this).find('span').removeClass('face-down');
     var cardOne = $(this).find('span').attr('class');
     console.log("card one value is "+ cardOne);
+
+    $('.col-md-3').click(function(clickTwo) {
+    $(this).find('span').removeClass('face-down');
+    var cardTwo = $(this).find('span').attr('class');
+    console.log("card two value is "+ cardTwo);
+
+    if (cardOne === cardTwo) {
+        console.log("they match!");
+    }
+});
 });
