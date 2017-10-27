f3f43398020c0b6697d70acdc99e39d42efbfce2
alffox <alfonso.crisci.it@gmail.com>
2017-10-27 11:35:34 +0200

    refactor: including and commenting out shuffle
    function from Udacity sample

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
+
+
+
+
+
+/* Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Used like so
+var arr = [2, 11, 37, 42];
+arr = shuffle(arr);
+console.log(arr);*/
+
+
+
+
+
+
+
+
 /*var mouseClicks = 0;
 
 $('.col-md-3').click(function() {
