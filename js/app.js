e5d2a24cc283763022d9855b4210461ea11e80c2
alffox <alfonso.crisci.it@gmail.com>
2017-10-31 11:51:15 +0100

    feat: Shuffle function

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@
 var cardsContainer = $("#deck");
 var cards = cardsContainer.children();
 var openCards = [];
+
+$(function () {
+    while (cards.length) {
+        cardsContainer.append(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
+    }
+});
