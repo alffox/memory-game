87819b28bf0423fc6a84aacf03d3dd4cd7e0e903
alffox <alfonso.crisci.it@gmail.com>
2017-10-31 11:52:00 +0100

    feat: Reveal card function

--- js/app.js
+++ js/app.js
@@ -8,3 +8,15 @@ $(function () {
         cardsContainer.append(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
     }
 });
+
+$(cards).click(function() {
+    var card = $(this);
+    //mouseClicks += 1;
+
+
+    //console.log("clicks count: "+ mouseClicks);
+
+    function revealCard() {
+        $(card).addClass('no-click-area');
+        $(card).removeClass('covered');
+    }
