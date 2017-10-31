40aff543af1a5c2b69d3925493210e1f92cf5f75
alffox <alfonso.crisci.it@gmail.com>
2017-10-31 11:53:48 +0100

    feat: Implement card matcher function

--- js/app.js
+++ js/app.js
@@ -13,10 +13,28 @@ $(cards).click(function() {
     var card = $(this);
     //mouseClicks += 1;
     revealCard();
-
+    cardMatcher();
     //console.log("clicks count: "+ mouseClicks);
 
     function revealCard() {
         $(card).addClass('no-click-area');
         $(card).removeClass('covered');
     }
+
+    function cardMatcher() {
+        openCards.push(card.attr('class'));
+        if (openCards.length > 1) {
+            console.log("card array is: " + openCards);
+            console.log("the list has more than 1 item");
+            for ( var i = 1; i < openCards.length; i++ ){
+                if(openCards[i-1] == openCards[i]) {
+                    console.log("there is a match !");
+                    openCards.splice(openCards[i], 2);
+                    console.log("new array is "+ openCards);
+                }
+            }
+        }
+    }
+
+
+});
